3e607ad148a93cd4f4f3cf70aa346950ca8c33cb
Keisuke2162 <applabo.kei@gmail.com>
2024-10-28 04:40:42 +0900

    マルチモジュール化

---
+++ FootballDataApp/FootballDataAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  FootballDataAppApp.swift
+//  FootballDataApp
+//
+//  Created by Kei on 2024/10/28.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FootballDataAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
